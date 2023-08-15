612c3406a69c0fa675c356845d4bfa5b730d2d25
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-15 12:33:25 +0300

    Completedassignment

--- lib/functions.py
+++ lib/functions.py
@@ -3,14 +3,14 @@
 def greet_programmer():
     print("Hello, programmer!")
 
-def greet(name ="Patience") :
+def greet(name) :
     print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 
-def add(num1=45, num2=4):
+def add(num1, num2):
     return  num1 + num2
 
-def halve(number=34):
+def halve(number):
     return number /2
